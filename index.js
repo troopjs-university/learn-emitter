c22e6951ad9d2bc1fbf254df1ee90c862863166a
Mikael Karon <mikael@karon.se>
2015-04-14 17:57:02 +0800

    Your first emit, now with effects
    So the reason nothing happened in the previous step is that we never added any handlers to the emitter, so let's go ahead and add a handler for `log` that will `.log` to the `console`.

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ require({
   "callback": function (Emitter) {
     var emitter = new Emitter();
 
+    emitter.on("log", function () {
+      console.log("This is a log message [%o]", arguments);
+    });
+
     emitter.emit("log", "string", 123, false);
   }
 });
