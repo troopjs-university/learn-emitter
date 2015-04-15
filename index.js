a772fa7579682cd10c67ec62faa472340188e848
Mikael Karon <mikael@karon.se>
2015-04-15 18:53:47 +0800

    Handler return values
    So it's nice to be able to observe and react to an emitter, but what if we wanted to return results to whoever called `.emit`? Have no fear, handler `return` values are here! The default executor collects results from each handler and returns that result from `.emit`.

--- index.js
+++ index.js
@@ -8,20 +8,24 @@ require({
 
     function one(a, b, c) {
       console.log("one [%o]", a);
+      return 1;
     }
 
     function two(a, b, c) {
       console.warn("two [%o]", b);
+      return 2;
     }
 
     function three(a, b, c) {
       console.info("three [%o]", c);
+      return 3;
     }
 
     emitter.on("console", one);
     emitter.on("console", two);
     emitter.on("console", three);
 
-    emitter.emit("console", "string", 123, false);
+    var emit = emitter.emit("console", "string", 123, false);
+    console.log(emit);
   }
 });
