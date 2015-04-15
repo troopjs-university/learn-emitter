98d1baeeafa3da1bb06ffc58c234039eae588b0c
Mikael Karon <mikael@karon.se>
2015-04-15 19:32:59 +0800

    Handler scope, binding
    However this can be changed by defining a `scope` during `.on`.

--- index.js
+++ index.js
@@ -4,20 +4,27 @@ require({
   "deps": [ "mu-emitter/main" ],
 
   "callback": function (Emitter) {
-    var emitter1 = new Emitter();
-    var emitter2 = new Emitter();
+    var emitter = new Emitter();
+    var scope1 = {};
+    var scope2 = {};
 
-    emitter1.name = "e1";
-    emitter2.name = "e2";
+    scope1.name = "e1";
+    scope2.name = "e2";
 
     function log() {
       console.log("%s [%o]", this.name, arguments);
     }
 
-    emitter1.on("console", log);
-    emitter2.on("console", log);
+    emitter.on("console", {
+      "scope": scope1,
+      "callback": log
+    });
 
-    emitter1.emit("console", "string", 123, false);
-    emitter2.emit("console", "string", 123, false);
+    emitter.on("console", {
+      "scope": scope2,
+      "callback": log
+    });
+
+    emitter.emit("console", "string", 123, false);
   }
 });
